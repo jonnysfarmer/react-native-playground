0f0a9b07f765669e4743bfac6133eb889382f5af
jonnysfarmer <john.farmer@latimerross.com>
2020-04-11 15:44:56 +0100

    basic list

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -2,20 +2,31 @@ import React from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
+  // Need to add a Key property to the object - must be a unique string...
+  // or you can add keyExtrator to Flatlist
   const friends = [
-    { name: 'friend 1' },
-    { name: 'friend 2' },
-    { name: 'friend 3' },
-    { name: 'friend 4' },
-    { name: 'friend 5' }
+    { name: 'friend 1', age: 5 },
+    { name: 'friend 2', age: 6 },
+    { name: 'friend 3', age: 8 },
+    { name: 'friend 4', age: 9  },
+    { name: 'friend 5', age: 10  },
+    { name: 'friend 6', age: 12  },
+    { name: 'friend 7', age: 15  }
   ]
 
   return (
+    // Use flatlist opposed to Map as its inbult into react native and a lot 
+    // more flexibke.  element is both item and postion in array.
+    //for we take { item } out.
     <FlatList
+      // horizontal
+      // showsHorizontalScrollIndicator = {false}
+      keyExtractor={(friend)=> friend.name}
+      // This basically gives the key.  It returns a unique string from the array of Objects
       data={friends}
       renderItem={({ item }) => {
         return (
-          <Text>{item.name}</Text>
+          <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
         )
       }}
     />
@@ -24,6 +35,9 @@ const ListScreen = () => {
 }
 
 const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 60
+  }
 
 })
 
