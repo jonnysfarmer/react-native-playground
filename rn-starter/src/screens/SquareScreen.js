eeb9c59e14b3f5542bcea7f328b1af5c889c39fa
jonnysfarmer <john.farmer@latimerross.com>
2020-04-16 12:16:56 +0100

    basic reducer done

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -9,14 +9,15 @@ const reducer = (state, action) => {
   // state === { red: number, green: number, blue: number}
   // action === { colorToChange: 'red' || 'green' || 'blue',  amount: -15 || +15
 
-  switch(action.colorToChange){
+  switch (action.colorToChange) {
     case 'red':
       return { ...state, red: state.red + action.amount }
     case 'green':
-        return { ...state, green: state.green + action.amount }
+      return { ...state, green: state.green + action.amount }
     case 'blue':
-        return { ...state, blue: state.blue + action.amount }
+      return { ...state, blue: state.blue + action.amount }
     default:
+      return state
   }
 }
 
@@ -24,22 +25,22 @@ const SquareScreen = () => {
 
 
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
-  // console.log(state) = { red: 0, green: 0, blue: 0 }
+  const {red, green, blue } = state
 
   return (
     <View>
       <Text>Square screen</Text>
       <ColorCounter 
-        onIncrease={()=> } 
-        onDecrease={()=> } 
+        onIncrease={()=> dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })} 
+        onDecrease={()=> dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })} 
         color='Red'/>
       <ColorCounter 
-        onIncrease={()=> } 
-        onDecrease={()=> } 
+        onIncrease={()=> dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })} 
+        onDecrease={()=> dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })} 
         color='Blue'/>
       <ColorCounter 
-        onIncrease={()=> } 
-        onDecrease={()=> } 
+        onIncrease={()=> dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })} 
+        onDecrease={()=> dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })} 
         color='Green'/>
       <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />
 
