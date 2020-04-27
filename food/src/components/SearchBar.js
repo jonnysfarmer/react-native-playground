9ef9175279c83110cf96f8b12819bdc639633139
jonnysfarmer <john.farmer@latimerross.com>
2020-04-27 10:54:22 +0100

    basic search bar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.iconStyle}/>
+      <TextInput 
+        placeholder='Search'
+        style={styles.inputStyle}
+      />
     </View>
 
   )
 }
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: '#F0EEEE'
+  backgroundStyle: {
+    backgroundColor: '#D8D6D6',
+    height: 50,
+    borderRadius: 5,
+    margin: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    // borderColor: 'black',
+    // borderWidth: 1,
+    flex: 1,
+    fontSize: 20
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 10
+
   }
+
 })
 
 export default SearchBar
