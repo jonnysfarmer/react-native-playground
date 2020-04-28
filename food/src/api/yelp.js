ebfa2f7e888144317e675555375db3ea6439dcc4
jonnysfarmer <john.farmer@latimerross.com>
2020-04-28 11:07:15 +0100

    api request yelp

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    // Authorization: 
+    Authorization: 'Bearer 5RzerkcmUcyK1bCzuxyVM-b5w6xeeYKia1De_Z2r2pAY0nfi5PAA2x-XZeWEb3-WrbMBrKhkZ7AknLWAjJOLf2gR2QIYc8MZcE4AbXTOOrvqN1-HSEBK6-yvD56lXnYx'
   }
 })
