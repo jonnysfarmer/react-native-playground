1fab8387884550fac0db5fef95650c1e2186ce71
jonnysfarmer <john.farmer@latimerross.com>
2020-04-28 15:58:31 +0100

    filter and displayer results

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 import SearchBar from '../components/SearchBar'
-
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, results, errorMessage] = useResults()
+
+  const filterResultsByPrice = (price) => {
+    // price === '$ || '$$' || '$$$'
+    return results.filter(result => {
+      console.log(result.price)
+      return result.price === price
+      
+    })
+  }
   
 
   return (
@@ -19,6 +28,10 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results.</Text>
+      <ResultsList title='Cost Effective' results={filterResultsByPrice('£')}/>
+      <ResultsList title='Bit Pricier' results={filterResultsByPrice('££')}/>
+      <ResultsList title='Big Spender' results={filterResultsByPrice('£££')}/>
+
     </View>
 
   )
