c51def87efad03cdffc7d65c8eb62dac80475741
jonnysfarmer <john.farmer@latimerross.com>
2020-06-16 15:18:08 +0100

    added edit to context reducer and sorted out edit pop navigation

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -5,6 +5,10 @@ const blogReducer = (state, action) => {
   switch (action.type){
     case 'add_blogpost':
       return [...state, { id: Math.floor(Math.random() * 9999), title: action.payload.title, content: action.payload.content }]
+    case 'edit_blogpost':
+      return state.map((blogPost)=> {
+        return blogPost.id === action.payload.id ? action.payload : blogPost
+      })
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload)
     default:
@@ -26,6 +30,12 @@ const deleteBlogPost = (dispatch)=> {
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 }
+const editBlogPost = (dispatch)=> {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id , title, content }})
+    callback()
+  }
+}
 
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'Test Post', content: 'Test Content', id: 1 }])
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'Test Post', content: 'Test Content', id: 1 }])
--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -4,16 +4,17 @@ import { Context } from '../context/BlogContext'
 import BlogPostForm from '../components/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
+  const id = navigation.getParam('id')
 
-  const { state } = useContext(Context)
+  const { state, editBlogPost } = useContext(Context)
 
-  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+  const blogPost = state.find((blogPost) => blogPost.id === id)
 
 
 
   return (
     <BlogPostForm 
-      onSubmit={(title, content)=> console.log(title)}
+      onSubmit={(title, content)=> editBlogPost(id, title, content, () => navigation.pop())}
       initialValues={{ title: blogPost.title, content: blogPost.content }}
     />
   )
